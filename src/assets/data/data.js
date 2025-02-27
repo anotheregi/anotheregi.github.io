1dcd8d8a6b281f4857be80510bc7c91527c56bbe
egi <egighidayat@gmail.com>
2025-02-27 17:04:41 +0700

    Update data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -75,15 +75,9 @@ export const data = {
     bank: [
         {
             id: 1,
-            name: 'Lorem Ipsum',
-            icon: './src/assets/images/bca.png',
-            rekening: '12345678'
-        },
-        {
-            id: 2,
-            name: 'Ipsum Lorem',
-            icon: './src/assets/images/bri.png',
-            rekening: '12345678'
+            name: 'DANA',
+            icon: './src/assets/images/dana.png',
+            rekening: '0812345678'
         },
     ],
 
